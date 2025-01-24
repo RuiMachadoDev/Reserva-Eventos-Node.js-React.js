5488e9a97283fef3b3cd7c4ac866385b68539011
RuiMachadoDev <ruifilipemachado@hotmail.com>
2025-01-24 15:43:41 +0000

    Atualização domficheiro restaurants do backend

--- backend/routes/restaurants.js
+++ backend/routes/restaurants.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const authenticateToken = require('../middlewares/auth');
 const db = require('../models/db');
 
+// Rota para listar todos os restaurantes
+router.get('/', authenticateToken, (req, res) => {
+    db.query('SELECT * FROM restaurants', (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar restaurantes:', err);
+            return res.status(500).json({ error: 'Erro ao buscar restaurantes' });
+        }
+        res.status(200).json(results);
+    });
+});
+
 // Rota para fazer uma reserva
 router.post('/:id/reservations', authenticateToken, (req, res) => {
     const restaurantId = req.params.id;
